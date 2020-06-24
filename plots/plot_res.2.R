380aa048e2fdf2307b55d9137ad4a3e9f9573da3
PaulMalliet <paul.malliet@sciencespo.fr>
2020-06-24 17:01:35 +0200

    Plot construction scripts
    script to make the plots with R from the ThreeME simulated results sheet

--- plots/plot_res.2.R
+++ plots/plot_res.2.R
@@ -13,7 +13,7 @@ police <- "Arial"
 getwd()
 user_path <- "/Users/paul/Documents/Professionnel/"
 path_res.plot <- str_c("ThreeME_V3/results/plots/")
-plot.dir <- "plot_res.1"
+plot.dir <- "plot_res.2"
 
 
 scenario <- c("TC", "COVID", "COVID_LOWOIL",  "COVID_LOWOIL_TC" )
@@ -21,35 +21,37 @@ scenario <- c("TC", "COVID", "COVID_LOWOIL",  "COVID_LOWOIL_TC" )
 
 
 for (frmt in c( "png", "pdf")){
-
+  
   dir.create(str_c(user_path,path_res.plot,frmt,"/", plot.dir,"/"), recursive = TRUE)
   
- date <-  "2020-06-24_08-29-13"
+  date <-  "2020-06-24_08-29-13"
 
   rep.plot <- list()
   for (sc in scenario){
-  rep.plot[[str_c("df_",sc)]] <- read.csv(str_c(user_path,"ThreeME_V3/results/allresults_",sc,".csv")) %>% rename(year = "X") %>%
-    filter(year >= 2015) 
+    rep.plot[[str_c("df_",sc)]] <- read.csv(str_c(user_path,"ThreeME_V3/results/allresults_",sc,".csv")) %>% rename(year = "X") %>%
+      filter(year >= 2015) 
   }
   
   data_plot <- rep.plot[[str_c("df_",sc)]] %>% select(year)
   for (sc in scenario){
-df.temp <- rep.plot[[str_c("df_",sc)]] %>%
-  select(contains("GDP_2")) %>%
-  `colnames<-`(str_c( "",sc)) 
- 
-data_plot <- cbind(data_plot, df.temp)
- }
-data_plot <- data_plot %>% gather(key = scenario, value = value, - year)
+    df.temp <- rep.plot[[str_c("df_",sc)]] %>%
+      select("X100..EMS_CO2_2.EMS_CO2_0.1.") %>%
+      `colnames<-`(str_c( "",sc)) 
+    data_plot <- cbind(data_plot, df.temp)
+  }
+  data_plot <- data_plot %>% gather(key = scenario, value = value, - year)
   
   plot <- ggplot(data =  data_plot, aes(x = year, y = value)) +
     geom_line(aes(linetype = scenario)) +
     scale_linetype_manual(values=c("solid", "twodash", "dotted", "longdash" )) +
+    ylim(-40, 20) + 
     theme_minimal() +
     theme(
       plot.title = element_text(size = 16, family = police),
       plot.subtitle = element_text(size = 8, family = police),
       legend.text= element_text(size = 10, family = police),
+      #legend.direction = "horizontal",
+      #legend.position=c(0.5, 1.05),
       axis.title.x = element_text(size = 16 ,family = police),
       axis.title.y = element_text(size = 12,family = police), 
       axis.text.x =  element_text( size = 10,family = police),
@@ -64,9 +66,9 @@ data_plot <- data_plot %>% gather(key = scenario, value = value, - year)
       x= "",
       y=" In %"
     )  
-
+  
   
   
   ggsave(str_c(plot.dir,".",frmt), plot, device = frmt, path = str_c(user_path, path_res.plot,frmt,"/", plot.dir), width = 300 , height = 150 , units = "mm", dpi = 600)
-    write.csv(data_plot,str_c(user_path,path_res.plot,frmt,"/", plot.dir,"/",plot.dir,".data.csv"))
+  write.csv(data_plot,str_c(user_path,path_res.plot,frmt,"/", plot.dir,"/",plot.dir,".data.csv"))
 }
--- plots/plot_res.3.R
+++ plots/plot_res.3.R
@@ -13,7 +13,7 @@ police <- "Arial"
 getwd()
 user_path <- "/Users/paul/Documents/Professionnel/"
 path_res.plot <- str_c("ThreeME_V3/results/plots/")
-plot.dir <- "plot_res.1"
+plot.dir <- "plot_res.3"
 
 
 scenario <- c("TC", "COVID", "COVID_LOWOIL",  "COVID_LOWOIL_TC" )
@@ -21,26 +21,24 @@ scenario <- c("TC", "COVID", "COVID_LOWOIL",  "COVID_LOWOIL_TC" )
 
 
 for (frmt in c( "png", "pdf")){
-
+  
   dir.create(str_c(user_path,path_res.plot,frmt,"/", plot.dir,"/"), recursive = TRUE)
   
- date <-  "2020-06-24_08-29-13"
-
-  rep.plot <- list()
+  date <-  "2020-06-24_08-29-13"
+  rep.plot$df_TC$F_L_2.F_L_0.1
   for (sc in scenario){
-  rep.plot[[str_c("df_",sc)]] <- read.csv(str_c(user_path,"ThreeME_V3/results/allresults_",sc,".csv")) %>% rename(year = "X") %>%
-    filter(year >= 2015) 
+    rep.plot[[str_c("df_",sc)]] <- read.csv(str_c(user_path,"ThreeME_V3/results/allresults_",sc,".csv")) %>% rename(year = "X") %>%
+      filter(year >= 2015) 
   }
   
   data_plot <- rep.plot[[str_c("df_",sc)]] %>% select(year)
   for (sc in scenario){
-df.temp <- rep.plot[[str_c("df_",sc)]] %>%
-  select(contains("GDP_2")) %>%
-  `colnames<-`(str_c( "",sc)) 
- 
-data_plot <- cbind(data_plot, df.temp)
- }
-data_plot <- data_plot %>% gather(key = scenario, value = value, - year)
+    df.temp <- rep.plot[[str_c("df_",sc)]] %>%
+      select("F_L_2.F_L_0.1") %>%
+      `colnames<-`(str_c( "",sc)) 
+    data_plot <- cbind(data_plot, df.temp)
+  }
+  data_plot <- data_plot %>% gather(key = scenario, value = value, - year)
   
   plot <- ggplot(data =  data_plot, aes(x = year, y = value)) +
     geom_line(aes(linetype = scenario)) +
@@ -50,6 +48,8 @@ data_plot <- data_plot %>% gather(key = scenario, value = value, - year)
       plot.title = element_text(size = 16, family = police),
       plot.subtitle = element_text(size = 8, family = police),
       legend.text= element_text(size = 10, family = police),
+      #legend.direction = "horizontal",
+      #legend.position=c(0.5, 1.05),
       axis.title.x = element_text(size = 16 ,family = police),
       axis.title.y = element_text(size = 12,family = police), 
       axis.text.x =  element_text( size = 10,family = police),
@@ -62,11 +62,11 @@ data_plot <- data_plot %>% gather(key = scenario, value = value, - year)
       subtitle =  "" ,
       caption="",
       x= "",
-      y=" In %"
+      y=" In thousands of workers"
     )  
-
+  
   
   
   ggsave(str_c(plot.dir,".",frmt), plot, device = frmt, path = str_c(user_path, path_res.plot,frmt,"/", plot.dir), width = 300 , height = 150 , units = "mm", dpi = 600)
-    write.csv(data_plot,str_c(user_path,path_res.plot,frmt,"/", plot.dir,"/",plot.dir,".data.csv"))
+  write.csv(data_plot,str_c(user_path,path_res.plot,frmt,"/", plot.dir,"/",plot.dir,".data.csv"))
 }
